96daceec1682accaf1d751d6ff74314f57db3db3
brunopicolotto <bruno.picolotto@compasso.com.br>
2022-07-27 12:00:07 -0300

    Display table with data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState } from 'react';
+import useUsersList from './hooks/useUsersList'
+import UsersTable from './components/UsersTable'
+import Modal from './components/Modal';
+import styles from './App.module.css'
+import { User } from './types'
 
-function App() {
+
+const App = () => {
+  const userList = useUsersList()
+  const [userSelected, setUserSelected] = useState<undefined | User>(undefined)
+  const [modalOpen, setModalOpen] = useState(false)
+  const toggleModalOpen = useCallback(() => setModalOpen(prev => !prev), [])
+  const selectUser = useCallback((user: User) => setUserSelected(user), [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.appContainer}>
+      <UsersTable userList={userList} selectUser={selectUser} toggleModal={toggleModalOpen}/>
+      {modalOpen && (
+        <Modal userSelected={userSelected} toggleModalOpen={toggleModalOpen} />
+      )}
     </div>
   );
 }
 
-export default App;
+export default App
